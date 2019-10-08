3c8d3299da916c654cba81e6015b652d38f31688
Yann Parent <yann.parent4@gmail.com>
2019-10-08 13:50:11 +0200

    basic setup

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -1,6 +1,17 @@
+ARTICLE = %w(der die das)
+PP = %w(ich du er wir ihr Sie)
+SEIN = %w(bin bist ist sind seid sind)
+NOUN = %w(kate hund freund)
+
 class PagesController < ApplicationController
-  skip_before_action :authenticate_user!, only: [:home]
+  skip_before_action :authenticate_user!, only: [:home, :exercice]
 
   def home
+  end
+
+  def exercice
+    number = rand(0..5)
+    @part1 = PP[number] + ' ' + SEIN[number] + ' mit'
+    @part2 = NOUN.sample
   end
 end

a0128aebe3cde2a0d57cfe5a9df133f286a32264
Yann Parent <yann.parent4@gmail.com>
2019-11-08 16:41:27 +0100

    Return if user has succeed or failed

--- app/controllers/progress_trackers_controller.rb
+++ app/controllers/progress_trackers_controller.rb
@@ -1,13 +1,17 @@
- class ProgressTrackersController < ApplicationController
+class ProgressTrackersController < ApplicationController
   def create
     ProgressTracker.create!(user_id: current_user.id, exercice_id: params[:exercice_id], counter: 1)
     redirect_to exercice_path(params[:exercice_id])
   end
 
   def update
-    progress_tracker = ProgressTracker.find(params[:id])
-    progress_tracker.counter = progress_tracker.counter + 1
-    progress_tracker.save
-    redirect_to exercice_path(params[:exercice_id])
+    @success = false
+    if params[:response].downcase == params[:solution]
+      progress_tracker = ProgressTracker.find(params[:id])
+      progress_tracker.counter = progress_tracker.counter + 1
+      progress_tracker.save
+      @success = true
+    end
+    redirect_to exercice_path(params[:exercice_id], success: @success, solution: params[:solution])
   end
 end

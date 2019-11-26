30fa663676aff42bb8c9a7fb6731f78c66cc5c63
Yann Parent <yann.parent4@gmail.com>
2019-11-26 20:08:09 +0100

    Full fix for the default profile picture

--- app/helpers/views_helper.rb
+++ app/helpers/views_helper.rb
@@ -1,7 +1,6 @@
 module ViewsHelper
   def profile_picture(user)
-
-    # return user.photo.url if user.photo
+    return user.photo.url if user.photo.url
 
     'https://res.cloudinary.com/duw2hqlo3/image/upload/v1574262704/pechrvswkps6hqjoovqr.jpg'
   end

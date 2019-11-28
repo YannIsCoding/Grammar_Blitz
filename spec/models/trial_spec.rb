40b31d95221274f3ee6e4f13f26a2f5a85388c21
Yann Parent <yann.parent4@gmail.com>
2019-11-28 16:33:21 +0100

    progression bar and styling

--- spec/models/trial_spec.rb
+++ spec/models/trial_spec.rb
@@ -2,8 +2,6 @@ require 'rails_helper'
 
 RSpec.describe Trial, type: :model do
   describe 'associations' do
-      it { should belong_to(:progress_tracker) }
-    end
-
+    it { should belong_to(:progress_tracker) }
   end
-
+end

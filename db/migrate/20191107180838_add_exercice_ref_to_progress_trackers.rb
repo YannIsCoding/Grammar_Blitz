43ef555ed429c40d4ca602f5b7b25dc40599727f
Yann Parent <yann.parent4@gmail.com>
2019-11-29 17:50:02 +0100

    Fixing travis

--- db/migrate/20191107180838_add_exercice_ref_to_progress_trackers.rb
+++ db/migrate/20191107180838_add_exercice_ref_to_progress_trackers.rb
@@ -1,6 +1,6 @@
-class AddExerciceRefToProgressTrackers < ActiveRecord::Migration[5.2]
-  def change
-    add_reference :progress_trackers, :exercice, foreign_key: true
-    add_reference :progress_trackers, :user, foreign_key: true
-  end
-end
+# class AddExerciceRefToProgressTrackers < ActiveRecord::Migration[5.2]
+#   def change
+#     add_reference :progress_trackers, :exercice, foreign_key: true
+#     add_reference :progress_trackers, :user, foreign_key: true
+#   end
+# end
--- db/migrate/20191108084721_add_counter_to_progress_trackers.rb
+++ db/migrate/20191108084721_add_counter_to_progress_trackers.rb
@@ -1,5 +1,5 @@
-class AddCounterToProgressTrackers < ActiveRecord::Migration[5.2]
-  def change
-    add_column :progress_trackers, :counter, :integer
-  end
-end
+# class AddCounterToProgressTrackers < ActiveRecord::Migration[5.2]
+#   def change
+#     add_column :progress_trackers, :counter, :integer
+#   end
+# end

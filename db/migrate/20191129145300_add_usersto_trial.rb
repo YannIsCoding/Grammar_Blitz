64e0df89b3effeece2f55b8bde414918d7d245aa
Yann Parent <yann.parent4@gmail.com>
2019-11-29 18:13:02 +0100

    Fixing travis

--- db/migrate/20191129145300_add_usersto_trial.rb
+++ db/migrate/20191129145300_add_usersto_trial.rb
@@ -2,6 +2,6 @@ class AddUserstoTrial < ActiveRecord::Migration[5.2]
   def change
     add_reference :trials, :user, foreign_key: true
     add_reference :trials, :exercice, foreign_key: true
-    remove_reference :trials, :progress_tracker, index: true, foreign_key: true
+    # remove_reference :trials, :progress_tracker, index: true, foreign_key: true
   end
 end

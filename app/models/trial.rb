351616e858cd1d38e2487e5786a3ad1f9b288358
Yann Parent <yann.parent4@gmail.com>
2019-11-30 19:21:13 +0100

    The ranking in the profile is now ordered by successes

--- app/models/trial.rb
+++ app/models/trial.rb
@@ -2,8 +2,4 @@ class Trial < ApplicationRecord
   belongs_to :user
   belongs_to :exercice
   validates :success, inclusion: { in: [true, false] }
-
-  def self.successes_count
-    where(success: true).count
-  end
 end

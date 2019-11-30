610cd475d9da3f55c997eaef0bf90ebb59c1abc0
Yann Parent <yann.parent4@gmail.com>
2019-11-30 10:37:34 +0100

    Ordering user by the count of successful tries

--- app/models/trial.rb
+++ app/models/trial.rb
@@ -2,4 +2,8 @@ class Trial < ApplicationRecord
   belongs_to :user
   belongs_to :exercice
   validates :success, inclusion: { in: [true, false] }
+
+  def self.successes_count
+    where(success: true).count
+  end
 end

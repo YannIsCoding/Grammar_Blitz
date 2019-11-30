dbef73dd5430c5f31445c3bdb2fee24eac92b6a0
Yann Parent <yann.parent4@gmail.com>
2019-11-30 11:18:15 +0100

    Exercice page: no success/failur message on new visit

--- app/controllers/exercices_controller.rb
+++ app/controllers/exercices_controller.rb
@@ -6,33 +6,30 @@ class ExercicesController < ApplicationController
   end
 
   def show
-    exercice_correction if params[:response]
     @exercice = Exercice.find(params[:id])
+    @exercice.result = false
+    exercice_correction if params[:response]
     @exercice.update_attributes SentenceBuilderService.new(@exercice).generate
     @exercice.save
-
-    # @progress_tracker = ProgressTracker.find_by(user: current_user, exercice: @exercice) || ProgressTracker.new
   end
 
   private
 
   def exercice_correction
-    @exercice = Exercice.find(params[:id])
-    @exercice.result = false
+    @exercice.result = true
     if params[:response]&.downcase == @exercice.solution[0]&.downcase && params[:response_2]&.downcase == @exercice.solution[1]&.downcase
-      successful_trial
+      successful_trial(true)
     elsif params[:response].downcase == @exercice.solution.first.downcase
-      successful_trial
+      successful_trial(true)
     else
-      Trial.create!(user: current_user, exercice: @exercice, success: false)
+      successful_trial(false)
     end
     @exercice.prev_sentence = @exercice.sentence
-    @exercice.save
-    redirect_to exercice_path(params[:id])
+    # @exercice.save
+    # redirect_to exercice_path(params[:id])
   end
 
-  def successful_trial
-    @exercice.result = true
-    Trial.create!(user: current_user, exercice: @exercice, success: true)
+  def successful_trial(success)
+    Trial.create!(user: current_user, exercice: @exercice, success: success)
   end
 end

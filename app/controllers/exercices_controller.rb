cc18c8919448daa1d555cdb404016e9e2f98febd
Yann Parent <yann.parent4@gmail.com>
2019-12-15 21:11:27 +0100

    trial session

--- app/controllers/exercices_controller.rb
+++ app/controllers/exercices_controller.rb
@@ -1,8 +1,6 @@
 require_relative '../services/sentence_builder_service'
 class ExercicesController < ApplicationController
   skip_before_action :authenticate_user!, only: :index
-  # before_action :set_exercice, only: [:show, :setup]
-  # before_action :set_sentence, only: [:show, :setup]
 
   def index
     @exercices = Exercice.all.order(:created_at)
@@ -13,7 +11,6 @@ class ExercicesController < ApplicationController
     last_sentence = Sentence.where(user: current_user, exercice: @exercice).last
     @sentence = Sentence.create(user: current_user, exercice: @exercice)
     @sentence.update(word_indexes: last_sentence.word_indexes) if last_sentence
-    # @sentence.update(word_indexes: Sentence.where(user: current_user, exercice: @exercice).last)
   end
 
   private

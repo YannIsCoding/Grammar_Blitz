b96bbabeadf8e7ec637fe76c8e3d50da9fae1e6d
Yann Parent <yann.parent4@gmail.com>
2020-01-08 10:49:21 +0100

    install of smtp mails

--- config/initializers/smtp.rb
+++ config/initializers/smtp.rb
@@ -1,9 +1,10 @@
 ActionMailer::Base.smtp_settings = {
-  domain:        'grammar-blitz.fun',
-  address:        "smtp.sendgrid.net",
-  port:            465,
+  from:           'moinmoin@grammar-blitz.fun',
   user_name:      'TM2DtlBvSF-mfoM3ucQacQ',
   password:       ENV['SENDGRID_API_KEY'],
-  authentication: :login,
+  domain:         'grammar-blitz.fun',
+  address:        "smtp.sendgrid.net",
+  port:            465,
+  authentication: :plain,
   enable_starttls_auto: true
 }

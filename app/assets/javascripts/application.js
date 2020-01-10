50fbc8678e66e775e0f950c9373510f48bbe5e23
Yann Parent <yann.parent4@gmail.com>
2020-01-10 10:56:08 +0100

    Separate screen for the solution

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -6,3 +6,27 @@ document.querySelectorAll(".form-label").forEach((label) => {
     event.currentTarget.classList.toggle("active-form")
   });
 });
+
+
+let button = document.getElementById("next-sentence")
+let solution = document.getElementById("solution")
+let exercice = document.getElementById("exercice")
+
+// if(button){
+//   button.focus();
+// }
+
+addEventListener("keypress", function(event){
+  solution.style.display = "none";
+  exercice.style.display = "block";
+  const firstField = document.querySelector("#response_0")
+  firstField.focus()
+})
+
+button.addEventListener("click", (event) => {
+  solution.style.display = "none";
+  exercice.style.display = "block";
+  const firstField = document.querySelector("#response_0")
+  firstField.focus()
+})
+
